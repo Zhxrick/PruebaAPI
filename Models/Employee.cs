238767d6a6d5b02a155b092076e8ced675650a5c
Zharick <zharickdonado848@gmail.com>
2024-08-26 22:58:29 -0500

    Init

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Prueba_Back.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+
+    }
+}
